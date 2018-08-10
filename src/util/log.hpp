19dc13fb0b89977ab95421f50767d2549374a803
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-08-10 14:40:35 +0300

    Add log level to log output
    Should make it easier to read logging output.
    Similar to what DXVK does.

--- src/util/log.hpp
+++ src/util/log.hpp
@@ -4,16 +4,19 @@
 namespace log {
     template <typename ...T>
     inline void error(T&&... params) {
+        std::cerr << "error:\t";
         (std::cerr << ... << std::forward<T>(params)) << '\n';
     }
 
     template <typename ...T>
     inline void warn(T&&... params) {
+        std::cerr << "warn:\t";
         (std::cerr << ... << std::forward<T>(params)) << '\n';
     }
 
     template <typename ...T>
     inline void info(T&&... params) {
+        std::cerr << "info:\t";
         (std::cerr << ... << std::forward<T>(params)) << '\n';
     }
 }
