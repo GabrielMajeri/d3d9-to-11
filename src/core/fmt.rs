30b2331212ccb0f6bf40d8ab8c34155cc086bc38
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-08-19 09:41:50 +0300

    Map unsupported formats to DXGI_FORMAT_UNKNOWN
    Instead of exposing support for them and hoping things work,
    it's better to just declare them and unsupported for now.

--- src/core/fmt.rs
+++ src/core/fmt.rs
@@ -47,15 +47,9 @@ macro_rules! format_conv {
     ($($a:path => $b:path,)*) => {
         /// Converts a general resource format to a DXGI format.
         pub fn d3d_format_to_dxgi(fmt: D3DFORMAT) -> DXGI_FORMAT {
+            #[allow(non_upper_case_globals)]
             match fmt {
                 $($a => $b,)*
-
-                // Mixed formats: not sure how to map these to DXGI
-                D3DFMT_L6V5U5 | D3DFMT_X8L8V8U8 | D3DFMT_A2W10V10U10 | D3DFMT_Q16W16V16U16 => {
-                    run_once!(|| warn!("Mixed sign formats are not supported"));
-
-                    DXGI_FORMAT_UNKNOWN
-                },
 
                 _ => panic!("Unknown D3D9 format: {}", fmt),
             }
@@ -76,27 +70,21 @@ macro_rules! format_conv {
 // Based upon the following reference:
 // https://docs.microsoft.com/en-us/windows/desktop/direct3d10/d3d10-graphics-programming-guide-resources-legacy-formats
 format_conv! {
-    // TODO: some formats have no support in modern DXGI.
-    // We might still be able to approximate them with some other formats though.
-
     // 8 bit formats
-    D3DFMT_R3G3B2 => DXGI_FORMAT_R8_UNORM,
     D3DFMT_A8 => DXGI_FORMAT_A8_UNORM,
     D3DFMT_L8 => DXGI_FORMAT_R8_UNORM,
 
     // 16 bit formats
-    D3DFMT_A8R3G3B2 => DXGI_FORMAT_R8G8_UNORM,
     D3DFMT_R5G6B5 => DXGI_FORMAT_B5G6R5_UNORM,
     D3DFMT_X4R4G4B4 => DXGI_FORMAT_B4G4R4A4_UNORM,
     D3DFMT_A4R4G4B4 => DXGI_FORMAT_B4G4R4A4_UNORM,
     D3DFMT_X1R5G5B5 => DXGI_FORMAT_B5G5R5A1_UNORM,
     D3DFMT_A1R5G5B5 => DXGI_FORMAT_B5G5R5A1_UNORM,
+    D3DFMT_A8L8 => DXGI_FORMAT_R8G8_UNORM,
     D3DFMT_L16 => DXGI_FORMAT_R16_UNORM,
 
-    // 24 bit formats
-    D3DFMT_R8G8B8 => DXGI_FORMAT_B8G8R8X8_UNORM,
-
     // 32 bit formats
+    D3DFMT_X8B8G8R8 => DXGI_FORMAT_R8G8B8A8_UNORM,
     D3DFMT_X8R8G8B8 => DXGI_FORMAT_B8G8R8X8_UNORM,
     D3DFMT_A8R8G8B8 => DXGI_FORMAT_B8G8R8A8_UNORM,
     D3DFMT_A8B8G8R8 => DXGI_FORMAT_R8G8B8A8_UNORM,
@@ -107,14 +95,10 @@ format_conv! {
     D3DFMT_A2B10G10R10 => DXGI_FORMAT_R10G10B10A2_UNORM,
 
     // Depth / stencil formats
-    D3DFMT_S8_LOCKABLE => DXGI_FORMAT_R8_UNORM,
     D3DFMT_D16_LOCKABLE => DXGI_FORMAT_D16_UNORM,
     D3DFMT_D16 => DXGI_FORMAT_D16_UNORM,
-    D3DFMT_D15S1 => DXGI_FORMAT_D24_UNORM_S8_UINT,
     D3DFMT_D24S8 => DXGI_FORMAT_D24_UNORM_S8_UINT,
-    D3DFMT_D24FS8 => DXGI_FORMAT_D24_UNORM_S8_UINT,
     D3DFMT_D24X8 => DXGI_FORMAT_D24_UNORM_S8_UINT,
-    D3DFMT_D24X4S4 => DXGI_FORMAT_D24_UNORM_S8_UINT,
     D3DFMT_D32 => DXGI_FORMAT_D32_FLOAT,
     D3DFMT_D32F_LOCKABLE => DXGI_FORMAT_D32_FLOAT,
 
@@ -143,6 +127,27 @@ format_conv! {
     D3DFMT_G32R32F => DXGI_FORMAT_R32G32_FLOAT,
     D3DFMT_A32B32G32R32F => DXGI_FORMAT_R32G32B32A32_FLOAT,
 
-    // Unknown formats
+    // Unknown format
     D3DFMT_UNKNOWN => DXGI_FORMAT_UNKNOWN,
+
+    // Unsupported formats
+    // TODO: some formats have no support in modern DXGI.
+    // We might still be able to approximate them with some other formats though.
+    D3DFMT_P8 => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_A8P8 => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_A4L4 => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_R3G3B2 => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_A8R3G3B2 => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_R8G8B8 => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_D15S1 => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_D24FS8 => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_D24X4S4 => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_S8_LOCKABLE => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_CxV8U8 => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_L6V5U5 => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_X8L8V8U8 => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_A2W10V10U10 => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_Q16W16V16U16 => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_UYVY => DXGI_FORMAT_UNKNOWN,
+    D3DFMT_YUY2 => DXGI_FORMAT_UNKNOWN,
 }
